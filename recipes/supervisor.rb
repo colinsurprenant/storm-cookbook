d93bb01a04a6a7e74bde7e55545fc2168fd0d8b2
TJ Vanderpoel <tj@rubyists.com>
2014-06-27 11:29:33 -0500

    add attribute to determine whether to start the storm processes or not

--- recipes/supervisor.rb
+++ recipes/supervisor.rb
@@ -17,4 +17,5 @@ bash "Start supervisor" do
     nohup apache-storm-#{node[:storm][:version]}/bin/storm supervisor >>supervisor.log 2>&1 &
   fi
   EOH
+  only_if { node[:storm][:deploy][:start_processes] }
 end
